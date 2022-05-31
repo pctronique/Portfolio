966e894e8f3de818a947b7bf30d1f16c2e7c5edf
Ludovic NAULOT <pctronique@free.fr>
2022-05-31 16:57:57 +0200

    début du code

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php 
+define("RACINE", "http://localhost:88/");
+define("RACINE_PATH", dirname(__FILE__)."/../../");
+define("DATA_PATH", dirname(__FILE__)."/../../data/");
+define("DATA_WEB", RACINE."data/");
+define("NAME_SITE", "Office du tourisme de Springfield");
+define("RACINE_MSG", RACINE."index.php?ind=msg");
+define("EMAIL_NOREPLY", "noreply@springfield.usa");
+
---
+++ project/www/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo file_get_contents(dirname(__FILE__) . '/src/templates/menu_footer_header.html', true);
